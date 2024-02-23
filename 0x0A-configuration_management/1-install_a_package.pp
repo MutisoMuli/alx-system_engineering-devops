215a495c38c892ab7cae45e137a68fba33c2e17f
MutisoMuli <mulimutiso525@gmail.com>
2024-02-23 23:00:34 +0300

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# A puppet manifest installing flask v2.1.0 a package of pip3
-# Version to install is specified using the ensure attribute
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
 package { 'flask':
   ensure   => '2.1.0',
   provider => pip3,
