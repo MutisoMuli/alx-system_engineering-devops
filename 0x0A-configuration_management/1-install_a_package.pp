1b4296714446f40ebc6dacc4c855dd8e0344f290
MutisoMuli <mulimutiso525@gmail.com>
2024-02-23 23:36:25 +0300

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,12 @@
 # Am also specifying the version of flask to install using the ensure attribute
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+
