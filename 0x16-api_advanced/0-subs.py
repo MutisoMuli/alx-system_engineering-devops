48a70986cd3f61dab9a284d6794f85bdbd491ee9
MutisoMuli <mulimutiso525@gmail.com>
2024-04-09 23:38:14 +0300

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,38 +1,27 @@
 #!/usr/bin/python3
 """
-This script queries the Reddit API and returns the number
-of subscribers for a given subreddit.
-If an invalid subreddit is given, the function returns 0.
+number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    Retrieve the number of subscribers for a given subreddit.
-    Args:
-        subreddit (str): The name of the subreddit.
-    Returns:
-        int: Number of subscribers of the subreddit, or 0 if invalid.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Chrome/99.0.9999.999 Safari/537.36'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=user_agent)
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
-    if response.status_code == 200:
-        try:
-            return response.json().get('data').get('subscribers')
-        except Exception:
-            pass
+    try:
+        return results.get('data').get('subscribers')
 
-    return 0
-
-
-if __name__ == "__main__":
-    subreddit_name = input("Enter subreddit name: ")
-    subscribers_count = number_of_subscribers(subreddit_name)
-    print(f"Number of subscribers in {subreddit_name}: {subscribers_count}")
+    except Exception:
+        return 0
