e03be5158cf34b2657e2c29b526fcfadbf02733a
MutisoMuli <mulimutiso525@gmail.com>
2024-04-21 00:49:07 +0000

    Done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,4 @@
-nx to accept and serve more requests
-
-exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+exec { 'modify_max_open_files_limit_setting':
+  command => 'sed -i "s/# worker_rlimit_nofile 1024;/worker_rlimit_nofile 10000;/" /etc/nginx/nginx.conf && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
